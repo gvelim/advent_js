7a0858d4adc0e9f40fccae08e61ae2ac5a1dc104
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-10 23:01:02 +0100

    written with fp-ts

--- day1/main.ts
+++ day1/main.ts
@@ -1,18 +1,20 @@
 
 import * as fs from "node:fs/promises";
+import {Option, some, none, match} from "fp-ts/Option";
+import {pipe} from "fp-ts/function";
 
 function isDigit(char: string): boolean {
     return char >= '0' && char<= '9'
 }
 
 const TXT = ["one","two","three","four","five","six","seven","eight","nine"];
-function scan_word(buf: string): (undefined|string) {
-    for (let i = 0; i < TXT.length; i++) {
-        if(buf.endsWith(TXT[i])) {
-            return (i+1).toString()
-        }
+
+function scan_word(buf: string): Option<string> {
+    for( let i = 0; i < TXT.length; i++ ) {
+        if( buf.endsWith(TXT[i]) )
+            return some((i+1).toString());
     }
-    return undefined;
+    return none;
 }
 
 let buf = await fs.readFile("./day1/sample.txt", {encoding: 'ascii'});
@@ -20,17 +22,20 @@ let buf = await fs.readFile("./day1/sample.txt", {encoding: 'ascii'});
 
 for (const line of buf.split("\n")) {
     let buf = [];
-    let last = 0;
+    let l = 0;
+
     for (let i = 0; i < line.length; i++) {
         if( isDigit(line[i]) ) {
-            last = i;
+            l = i+1;
             buf.push(line[i]);
         } else {
-            let ret = scan_word(line.substring(last,i+1));
-            if( typeof ret === "string"  ) {
-                last = i;
-                buf.push(ret)
-            }
+            pipe(
+                scan_word(line.substring(l,i+1)),
+                match(
+                    () => {},
+                    (val) => { l = i; buf.push(val) }
+                ),
+            )
         }
     }
     console.log(line + " = " + buf[0] + buf.pop());
