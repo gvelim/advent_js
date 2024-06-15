89f706e024779d771e729275e7f392b27b8660af
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-15 15:10:41 +0100

    iterator

--- day1/main.ts
+++ day1/main.ts
@@ -2,6 +2,7 @@
 import * as fs from "node:fs/promises";
 import {Option, some, none, map} from "fp-ts/Option";
 import {pipe} from "fp-ts/function";
+import { nextTick } from "node:process";
 
 const isDigit = (char: string): boolean => {
     return char >= '0' && char<= '9'
@@ -17,13 +18,40 @@ const scan_word = (buf: string): Option<string> => {
     return none;
 }
 
-let buf = await fs.readFile("./day1/input.txt", {encoding: 'ascii'});
+let buf = await fs.readFile("./day1/sample.txt", {encoding: 'ascii'});
 let sum = 0;
+
+function ParseIterator(line:string) {
+    let l = 0;
+    let i = 0;
+
+    return {
+        next: () => {
+            let ret: string = "";
+            while( ret === "" && i < line.length) {
+                if( isDigit(line[i]) ) {
+                    l = i+1,
+                    ret = line[i]
+                } else
+                    pipe(
+                    line.substring(l,i+1),
+                    scan_word,
+                    map((val) => { l = i; ret = val }),
+                    );
+                i++;
+            }
+            return {
+                done: !ret,
+                value: ret !== "" ? ret: undefined
+            };
+        }
+    }
+}
 
 console.time();
 for( const line of buf.split("\n") ) {
-    let buf = [];
-    let l = 0;
+  let buf = [];
+  let l = 0;
 
     for( let i = 0; i < line.length; i++ ) {
         if( isDigit(line[i]) ) {
@@ -35,7 +63,6 @@ for( const line of buf.split("\n") ) {
                 scan_word,
                 map((val) => { l = i; buf.push(val) }),
             );
-
     }
     sum += buf[0] ? parseInt(buf[0] + buf.pop()) : 0;
     console.log(line + " = " + sum);
@@ -43,3 +70,10 @@ for( const line of buf.split("\n") ) {
 
 console.log("Total = " + sum);
 console.timeEnd();
+
+let p = ParseIterator("abcone2threexyz");
+
+console.log(p.next())
+console.log(p.next())
+console.log(p.next())
+console.log(p.next())
