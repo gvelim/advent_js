685ab4668b01ee5d6b3ecf5fda1b65cbeaa39eb3
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-17 22:07:02 +0100

    take split() off function

--- day1/main.ts
+++ day1/main.ts
@@ -1,33 +1,27 @@
 import * as fs from "node:fs/promises";
-import {Parser, Part1ParserGen, Part2ParserGen, Part2ParserIter} from "../day1/parser";
+import {Parser, Part1ParserGen, Part2ParserGen, Part2ParserIter} from "./parser";
 
-let buf = await fs.readFile("./day1/input.txt", { encoding: "ascii" });
+let lines = (await fs.readFile("./day1/input.txt", { encoding: "ascii" })).split("\n");
 
-const summarise = (buffer: string, parser: Parser): number => {
+const summarise = (lines: string[], parser: Parser): number => {
     let sum = 0;
-    buffer
-        .split("\n")
-        .forEach( (line) => {
-            let buf = new Array<string>;
-            Array
-                .from(parser.parse(line))
-                .forEach(
-                    (n) => buf.push(n)
-                );
-            sum += buf[0] ? parseInt(buf[0] + buf.pop()) : 0;
-            // console.log(line + " = " + sum);
-        });
+    lines.forEach( (line) => {
+        let buf = new Array<string>;
+        for(let n of parser.parse(line)) buf.push(n);
+        sum += buf[0] ? parseInt(buf[0] + buf.pop()) : 0;
+        // console.log(line + " = " + sum);
+    });
     return sum;
 };
 
 console.time();
-console.log("\nPart1ParserGen Total = " + summarise(buf, new Part1ParserGen()));
+console.log("\nPart1ParserGen Total = " + summarise(lines, new Part1ParserGen()));
 console.timeEnd();
 
 console.time();
-console.log("\nPart2ParserIter Total = " + summarise(buf, new Part2ParserIter()));
+console.log("\nPart2ParserIter Total = " + summarise(lines, new Part2ParserIter()));
 console.timeEnd();
 
 console.time();
-console.log("\nPart2ParserGen Total = " + summarise(buf, new Part2ParserGen()));
+console.log("\nPart2ParserGen Total = " + summarise(lines, new Part2ParserGen()));
 console.timeEnd();
