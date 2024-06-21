6f3ac25d8629621b6379dd28bb39aa662f85cb5f
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-21 21:46:41 +0100

    basic parsing

--- day2/index.ts
+++ day2/index.ts
@@ -1,5 +1,47 @@
 import * as fs from "node:fs/promises";
 
+
+class Run {
+    red? :number;
+    green? :number;
+    blue? :number;
+
+    constructor(input: string) {
+        input
+            .split(",")
+            .forEach(
+                (run) => {
+                    const r = run.trim().split(" ");
+                    switch(r[1]) {
+                        case "red": this.red = parseInt(r[0]); break;
+                        case "green": this.green = parseInt(r[0]); break;
+                        case "blue": this.blue = parseInt(r[0]); break;
+                    }
+                }
+            )
+    }
+}
+
+class Game {
+    id: number;
+    runs: Array<Run> = [];
+
+    constructor(input: string) {
+        let g = input.split(":");
+        this.id = parseInt(g[0].split(" ")[1]);
+        g[1].trim()
+            .split(";")
+            .forEach(
+                (run) => this.runs.push(new Run(run))
+            )
+    }
+}
+
+
 let buf = await fs.readFile("./day2/sample.txt", { encoding: "ascii" });
 
-console.log(buf);
+buf
+  .split("\n")
+  .forEach(
+        (line) => console.log(new Game(line))
+    );
