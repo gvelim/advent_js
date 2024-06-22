8f3bd978e6694524271794709f4b3a846b385a5a
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-22 14:57:20 +0100

    cosmetics

--- day2/index.ts
+++ day2/index.ts
@@ -2,26 +2,21 @@ import * as fs from "node:fs/promises";
 import { InputParse } from "./parser";
 import { Run } from "./game";
 
-let buf = await fs.readFile("./day2/input.txt", { encoding: "ascii" });
-
-let games = new InputParse().parse(buf);
+let games = new InputParse()
+    .parse(
+        await fs.readFile("./day2/input.txt", { encoding: "ascii" })
+    );
 
 if( games._tag === "None" ) {}
 else {
     const ref_run = new Run("12 red, 13 green, 14 blue");
     let sum = games.value
-        .map((game) =>
-            game.runs.every((r) => r.possible(ref_run)) ? game.id : 0
-        )
+        .map((game) => game.runs.every((r) => r.possible(ref_run)) ? game.id : 0)
         .reduce((sum, id) => sum + id);
     console.log("Part 1: " + sum);
 
     sum = games.value
-        .map((game) =>
-            game.runs
-                .reduce((max, run) => max.maximum(run))
-                .power()
-        )
+        .map((game) => game.runs.reduce((max, run) => max.maximum(run)).power())
         .reduce((sum, pwr) => sum + pwr)
     console.log("Part 2: " + sum);
 }
