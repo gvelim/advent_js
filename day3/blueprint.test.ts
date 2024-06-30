4bc7c90692cf9537160cc6b2bbb2daefe14739cd
gvelim <3185416+gvelim@users.noreply.github.com>
2024-06-30 21:13:19 +0100

    test unit test

--- day3/blueprint.test.ts
+++ day3/blueprint.test.ts
@@ -1,12 +1,27 @@
-import { Part, Blueprint } from "./blueprint";
+import * as fs from "node:fs/promises";
+import { Blueprint, Part } from "./blueprint";
+import {throws} from "node:assert";
+
+async function load_data() {
+    return await fs.readFile("./day3/sample.txt",{ encoding: "ascii"})
+}
+test("Blueprint::parse", () => {
+    load_data().then(
+        (input) => {
+            let bp = Blueprint.parse(input);
+            expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
+        })
+});
 
 test( "Part::is_touching()", () => {
-    let r1 = new Part("432",0,2);
-    let r2 = new Part("*",13,13);
-    expect(
-        r1.pos.contains(r2.pos,10)
-    ).toBe(true);
-    expect(
-     r1.is_touching(r2, 10)
-    ).toBe(true);
+    load_data().then(
+        (input) => {
+            let bp = Blueprint.parse(input);
+            expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
+
+            expect(bp.parts[0].is_touching(bp.symbols[0], bp.step)).toBe(true);
+            expect(bp.parts[1].is_touching(bp.symbols[0], bp.step)).toBe(false);
+            expect(bp.parts[4].is_touching(bp.symbols[2], bp.step)).toBe(true);
+            expect(bp.parts[2].is_touching(bp.symbols[2], bp.step)).toBe(false);
+        });
 });
