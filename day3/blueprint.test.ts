e80cfbbbc1adc8071c64a29d6e0b36ba2c75b854
gvelim <3185416+gvelim@users.noreply.github.com>
2024-07-04 19:00:14 +0100

    vitest + unit test change()

--- day3/blueprint.test.ts
+++ day3/blueprint.test.ts
@@ -1,3 +1,4 @@
+import {expect, test} from "vitest";
 import * as fs from "node:fs/promises";
 import { Blueprint } from "./blueprint";
 
@@ -5,36 +6,24 @@ async function load_data() {
     return await fs.readFile("./day3/sample.txt",{ encoding: "ascii"})
 }
 
-test("Blueprint::Iterator", () => {
-    load_data().then( (input) => {
-        let iter = Blueprint.parse(input).gears("*");
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-        expect(iter.next().value).toHaveLength(2);
-    })
+test("Blueprint::Iterator", async () => {
+    let iter = Blueprint.parse(await load_data()).gears("*");
+    expect(iter.next().value).toHaveLength(2);
+    expect(iter.next().value).toHaveLength(2);
+    expect(iter.next().value).toHaveLength(0);
 });
 
-test("Blueprint::parse", () => {
-    load_data().then(
-        (input) => {
-            let bp = Blueprint.parse(input);
-            expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
-        })
+test("Blueprint::parse", async () => {
+    let bp = Blueprint.parse(await load_data());
+    expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
 });
 
-test( "Part::is_touching()", () => {
-    load_data().then(
-        (input) => {
-            let bp = Blueprint.parse(input);
-            expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
+test( "Part::is_touching()", async () => {
+    let bp = Blueprint.parse(await load_data());
+    expect(bp.parts.length && bp.symbols.length).toBeGreaterThan(0);
 
-            expect(bp.parts[0].is_touching(bp.symbols[0], bp.step)).toBe(true);
-            expect(bp.parts[1].is_touching(bp.symbols[0], bp.step)).toBe(false);
-            expect(bp.parts[4].is_touching(bp.symbols[2], bp.step)).toBe(true);
-            expect(bp.parts[2].is_touching(bp.symbols[2], bp.step)).toBe(false);
-        });
+    expect(bp.parts[0].is_touching(bp.symbols[0], bp.step)).toBe(true);
+    expect(bp.parts[1].is_touching(bp.symbols[0], bp.step)).toBe(false);
+    expect(bp.parts[4].is_touching(bp.symbols[2], bp.step)).toBe(true);
+    expect(bp.parts[2].is_touching(bp.symbols[2], bp.step)).toBe(false);
 });
