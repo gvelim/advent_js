54861a67024556d3f7624c822a18ea9c791d4984
gvelim <3185416+gvelim@users.noreply.github.com>
2024-07-12 18:20:35 +0100

    refactor test

--- day2/game.test.ts
+++ day2/game.test.ts
@@ -2,23 +2,25 @@ import {test, expect} from "vitest";
 import {parse_run} from "./parser.js";
 import {Option} from "fp-ts/Option";
 import {Run} from "./game.js";
+import {some} from "fp-ts/Option";
+
+function unwrap<T>(val: Option<T>) : T | undefined {
+    return val._tag === "Some" ? val.value : undefined;
+}
 
 test("game::is_possible", () => {
-    let runs = [
-        parse_run("3 blue, 4 red"),
-        parse_run("1 red, 2 green, 6 blue"),
-        parse_run("2 green"),
-        parse_run("14 red, 2 green, 16 blue")
+    let r: Array<[Run,boolean]> = [
+        [parse_run("3 blue, 4 red"), true],
+        [parse_run("1 red, 2 green, 6 blue"), true],
+        [parse_run("2 green"), true],
+        [parse_run("4 red, 22 green, 6 blue"), false],
+        [parse_run("14 red, 22 green, 16 blue"), false],
+        [parse_run("4 red, 2 green, 16 blue"), false]
     ]
-        .filter((r) => r._tag === "Some")
-        .map((r) => r.value);
+        .filter((r) => r[0]._tag === "Some")
+        .map((r) => [r[0].value, r[1]]);
 
-    let unwrap = <T>(val: Option<T>) => val._tag === "Some" ? val.value : new Run();
-
-    let test = unwrap(parse_run("12 red, 13 green, 14 blue"));
-
-    expect(runs[0].is_possible(test)).toBe(true);
-    expect(runs[1].is_possible(test)).toBe(true);
-    expect(runs[2].is_possible(test)).toBe(true);
-    expect(runs[3].is_possible(test)).toBe(false);
+    let t = unwrap(parse_run("12 red, 13 green, 14 blue"));
+    for( let d of r)
+        expect(t && d[0].is_possible(t)).toBe(d[1]);
 });
