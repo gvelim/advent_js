bb50a4e4058156070bef82c51841b91bc3186dd8
gvelim <3185416+gvelim@users.noreply.github.com>
2024-07-16 21:40:08 +0100

    refactor test

--- day2/game.test.ts
+++ day2/game.test.ts
@@ -2,25 +2,24 @@ import {test, expect} from "vitest";
 import {parse_run} from "./parser.js";
 import {Option} from "fp-ts/Option";
 import {Run} from "./game.js";
-import {some} from "fp-ts/Option";
 
 function unwrap<T>(val: Option<T>) : T | undefined {
     return val._tag === "Some" ? val.value : undefined;
 }
 
 test("game::is_possible", () => {
-    let r: Array<[Run,boolean]> = [
-        [parse_run("3 blue, 4 red"), true],
-        [parse_run("1 red, 2 green, 6 blue"), true],
-        [parse_run("2 green"), true],
-        [parse_run("4 red, 22 green, 6 blue"), false],
-        [parse_run("14 red, 22 green, 16 blue"), false],
-        [parse_run("4 red, 2 green, 16 blue"), false]
+    const r: Array<{in:Run, out:boolean}> = [
+        {in : parse_run("3 blue, 4 red"), out : true},
+        {in : parse_run("1 red, 2 green, 6 blue"), out : true},
+        {in : parse_run("2 green"), out : true},
+        {in : parse_run("4 red, 22 green, 6 blue"), out : false},
+        {in : parse_run("14 red, 22 green, 16 blue"), out : false},
+        {in : parse_run("4 red, 2 green, 16 blue"), out : false}
     ]
-        .filter((r) => r[0]._tag === "Some")
-        .map((r) => [r[0].value, r[1]]);
+    .filter((r) => r.in._tag === "Some")
+    .map((r) => { return { in: r.in.value, out: r.out} });
 
-    let t = unwrap(parse_run("12 red, 13 green, 14 blue"));
+    const t = unwrap(parse_run("12 red, 13 green, 14 blue"));
     for( let d of r)
-        expect(t && d[0].is_possible(t)).toBe(d[1]);
+        expect(t && d.in.is_possible(t)).toBe(d.out);
 });
