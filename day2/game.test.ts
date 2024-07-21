507d7f890709f1ff6e5e2d36f5478fc3e42c94ae
gvelim <3185416+gvelim@users.noreply.github.com>
2024-07-21 13:45:11 +0100

    refactor test using test.each()

--- day2/game.test.ts
+++ day2/game.test.ts
@@ -7,22 +7,19 @@ function unwrap<T>(val: Option<T>) : T | undefined {
     return val._tag === "Some" ? val.value : undefined;
 }
 
-test("game::is_possible", () => {
-    const r: Array<{in:Run|undefined, out:boolean}> = [
-        {in : unwrap(parse_run("3 blue, 4 red")), out : true},
-        {in : unwrap(parse_run("1 red, 2 green, 6 blue")), out : true},
-        {in : unwrap(parse_run("2 green")), out : true},
-        {in : unwrap(parse_run("4 red, 22 green, 6 blue")), out : false},
-        {in : unwrap(parse_run("14 red, 22 green, 16 blue")), out : false},
-        {in : unwrap(parse_run("4 red, 2 green, 16 blue")), out : false}
-    ];
-
-    const t = unwrap(parse_run("12 red, 13 green, 14 blue"));
-    for( const d of r) {
-        assert.ok(d.in, "game::is_possible => Ops! got undefined instead of Run object");
-        expect(t && d.in && d.in.is_possible(t)).toBe(d.out);
-    }
-});
+// try test case as nested array
+test.each([
+    [unwrap(parse_run("3 blue, 4 red")), true],
+    [unwrap(parse_run("1 red, 2 green, 6 blue")), true],
+    [unwrap(parse_run("2 green")), true],
+    [unwrap(parse_run("4 red, 22 green, 6 blue")), false],
+    [unwrap(parse_run("14 red, 22 green, 16 blue")), false],
+    [unwrap(parse_run("4 red, 2 green, 16 blue")), false]
+])('game::is_possible: %o => %o', (inp, out) => {
+        const t = unwrap(parse_run("12 red, 13 green, 14 blue"));
+        assert.ok(inp, "game::is_possible => Ops! got undefined instead of Run object");
+        expect(t && inp && inp.is_possible(t)).toBe(out);
+    })
 
 test("game::fewest_feasible", () => {
     const r: Array<Run|undefined> = [
@@ -42,19 +39,16 @@ test("game::fewest_feasible", () => {
     assert.ok(f && t?.equals(t,f));
 });
 
-test("game::power",() => {
-    const r: Array<{in:Run|undefined, out:number}> = [
-        {in : unwrap(parse_run("3 red, 3 green, 3 blue")), out : 27},
-        {in : unwrap(parse_run("3 green, 3 blue")), out : 9},
-        {in : unwrap(parse_run("3 red,3 blue")), out : 9},
-        {in : unwrap(parse_run("3 red, 3 green")), out : 9},
-        {in : unwrap(parse_run("3 green")), out : 3},
-        {in : unwrap(parse_run("3 red")), out : 3},
-        {in : unwrap(parse_run("3 blue")), out : 3}
-    ];
-
-    for( const d of r) {
-        assert.ok(d.in, "game::power => Ops! got undefined instead of Run object");
-        expect( d.in && d.in.power()).toBe(d.out);
-    }
+// try test case as object
+test.each([
+    {in : unwrap(parse_run("3 red, 3 green, 3 blue")), out : 27},
+    {in : unwrap(parse_run("3 green, 3 blue")), out : 9},
+    {in : unwrap(parse_run("3 red,3 blue")), out : 9},
+    {in : unwrap(parse_run("3 red, 3 green")), out : 9},
+    {in : unwrap(parse_run("3 green")), out : 3},
+    {in : unwrap(parse_run("3 red")), out : 3},
+    {in : unwrap(parse_run("3 blue")), out : 3},
+])("game::power %o",(d) => {
+    assert.isOk(d.in, "game::power => Ops! got undefined instead of Run object");
+    expect( d.in && d.in.power()).toBe(d.out);
 });
