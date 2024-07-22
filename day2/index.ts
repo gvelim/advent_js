72283527b4d7a6ae4492e2ff07afb09e9d2e3e69
gvelim <3185416+gvelim@users.noreply.github.com>
2024-07-22 20:27:01 +0100

    cosmetic/readbility

--- day2/index.ts
+++ day2/index.ts
@@ -6,20 +6,31 @@ let games = parse_input(
         await fs.readFile("./day2/input.txt", { encoding: "ascii" })
     );
 
-if( games._tag === "None" ) {}
+if( games._tag === "None" ) {
+    console.log("main(): Error loading input data. Check input.txt exists under ./day2");
+}
 else {
     console.time();
     const ref_run = parse_run("12 red, 13 green, 14 blue");
-    let sum = (ref_run._tag === "Some") ? games.value
-        .map( (game) => game.runs.every((r) => r.is_possible(ref_run.value)) ? game.id : 0 )
-        .reduce( (sum, id) => sum + id )
+    let sum = (ref_run._tag === "Some")
+        ? games.value
+            .map(
+                (game) => game.runs.every(
+                    (r) => r.is_possible(ref_run.value)
+                ) ? game.id : 0
+            )
+            .reduce( (sum, id) => sum + id )
         : -1;
     console.log("Part 1: " + sum);
     console.timeEnd();
 
     console.time();
     sum = games.value
-        .map( (game) => game.runs.reduce( (few, run) => few.fewest_feasible(run) ).power() )
+        .map(
+            (game) => game.runs.reduce(
+                (few, run) => few.fewest_feasible(run)
+            ).power()
+        )
         .reduce( (sum, pwr) => sum + pwr )
     console.log("Part 2: " + sum);
     console.timeEnd();
