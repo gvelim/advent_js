fc5c213fb281d0c6c537d27a75a7e1845c2c7788
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-04 11:09:35 +0100

    refactor with pipe

--- day2/game.test.ts
+++ day2/game.test.ts
@@ -1,16 +1,16 @@
 import {test, expect} from 'vitest';
 import {parse_run} from './parser.ts';
-import {Option, Equal, Data} from 'effect';
+import {Option, Equal, Data, pipe} from 'effect';
 import {Run} from './game.ts';
 
 // try test case as nested array
 test.each([
-    [Option.getOrThrow(parse_run("3 blue, 4 red")), true],
-    [Option.getOrThrow(parse_run("1 red, 2 green, 6 blue")), true],
-    [Option.getOrThrow(parse_run("2 green")), true],
-    [Option.getOrThrow(parse_run("4 red, 22 green, 6 blue")), false],
-    [Option.getOrThrow(parse_run("14 red, 22 green, 16 blue")), false],
-    [Option.getOrThrow(parse_run("4 red, 2 green, 16 blue")), false]
+    [pipe(parse_run("3 blue, 4 red"),Option.getOrThrow), true],
+    [pipe(parse_run("1 red, 2 green, 6 blue"),Option.getOrThrow), true],
+    [pipe(parse_run("2 green"),Option.getOrThrow), true],
+    [pipe(parse_run("4 red, 22 green, 6 blue"),Option.getOrThrow), false],
+    [pipe(parse_run("14 red, 22 green, 16 blue"),Option.getOrThrow), false],
+    [pipe(parse_run("4 red, 2 green, 16 blue"),Option.getOrThrow), false]
 ])('game::is_possible: %o => %o', (inp:Run, out:boolean) => {
         const t = Option.getOrThrow(parse_run("12 red, 13 green, 14 blue"));
         expect(inp.is_possible(t)).toBe(out);
@@ -18,12 +18,12 @@ test.each([
 
 test("game::fewest_feasible", () => {
     const r: Array<Run> = [
-        Option.getOrThrow(parse_run("3 blue, 4 red")),
-        Option.getOrThrow(parse_run("1 red, 2 green, 6 blue")),
-        Option.getOrThrow(parse_run("2 green")),
-        Option.getOrThrow(parse_run("4 red, 22 green, 6 blue")),
-        Option.getOrThrow(parse_run("14 red, 21 green, 15 blue")),
-        Option.getOrThrow(parse_run("4 red, 2 green, 16 blue"))
+        pipe(parse_run("3 blue, 4 red"),Option.getOrThrow),
+        pipe(parse_run("1 red, 2 green, 6 blue"),Option.getOrThrow),
+        pipe(parse_run("2 green"),Option.getOrThrow),
+        pipe(parse_run("4 red, 22 green, 6 blue"),Option.getOrThrow),
+        pipe(parse_run("14 red, 21 green, 15 blue"),Option.getOrThrow),
+        pipe(parse_run("4 red, 2 green, 16 blue"),Option.getOrThrow)
     ];
 
     const t = Option.getOrThrow(parse_run("14 red, 22 green, 16 blue"));
@@ -34,6 +34,7 @@ test("game::fewest_feasible", () => {
     expect(Equal.equals(f,t)).toBe(true);
 });
 
+interface IO<IN,OUT> { in:IN, out:OUT };
 // try test case as object
 test.each([
     {in : Option.getOrThrow(parse_run("3 red, 3 green, 3 blue")), out : 27},
@@ -43,6 +44,6 @@ test.each([
     {in : Option.getOrThrow(parse_run("3 green")), out : 3},
     {in : Option.getOrThrow(parse_run("3 red")), out : 3},
     {in : Option.getOrThrow(parse_run("3 blue")), out : 3}
-])("game::power %o",(d: {in:Run,out:number}) => {
+])("game::power %o",(d: IO<Run,number>) => {
     expect( d.in && d.in.power()).toBe(d.out);
 });
--- day2/parser.test.ts
+++ day2/parser.test.ts
@@ -1,13 +1,13 @@
 import {test,expect} from 'vitest';
 import {parse_run} from './parser.ts';
-import { Option } from 'effect';
+import { Option, pipe } from 'effect';
 
 test.each([
-    Option.isSome(parse_run("1 green, 3 red, 6 blue")),
-    Option.isSome(parse_run("3 green, 6 red")),
-    Option.isSome(parse_run("1 green")),
-    Option.isSome(parse_run("1 green 3 red, 6 blue")),
-    Option.isNone(parse_run("1 green red, 6 blue")),
-    Option.isNone(parse_run("green, 3 red, 6 blue")),
-    Option.isNone(parse_run("1 green, 3 red, 6"))
+    pipe(parse_run("1 green, 3 red, 6 blue"),Option.isSome),
+    pipe(parse_run("3 green, 6 red"),Option.isSome),
+    pipe(parse_run("1 green"),Option.isSome),
+    pipe(parse_run("1 green 3 red, 6 blue"),Option.isSome),
+    pipe(parse_run("1 green red, 6 blue"),Option.isNone),
+    pipe(parse_run("green, 3 red, 6 blue"),Option.isNone),
+    pipe(parse_run("1 green, 3 red, 6"),Option.isNone)
 ])("parser::parse_run", (c) => expect(c).toBe(true))
