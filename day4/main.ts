272fb2e5b6c7554628f3ef0e8d22b6d098f9a929
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-11 00:06:37 +0100

    set interesection and card score

--- day4/main.ts
+++ day4/main.ts
@@ -3,29 +3,34 @@ import { pipe } from "effect/Function";
 
 interface ScratchCard {
     card: number;
-    draw: number[];
-    numbers: number[];
+    draw: Set<number>;
+    numbers: Set<number>;
 }
 
-const str_num_array = (line:string): number[] => line
+const str_num_array = (line :string): number[] => line
     .trim()
     .split(/\s+/)
     .filter((l) => l.length != 0)
-    .map((l) => Number(l));
+    .map((l) => parseInt(l));
 
-const parse_scratch_card = (line:string): ScratchCard => pipe(
+const parse_scratch_card = (line :string): ScratchCard => pipe(
     line
         .split(/^Card|:|\|/)
         .filter((l) => l.length),
     (ns): ScratchCard => ({
             card: str_num_array(ns[0].trim())[0],
-            draw: str_num_array(ns[1].trim()),
-            numbers: str_num_array(ns[2].trim()),
+            draw: new Set(str_num_array(ns[1].trim())),
+            numbers: new Set(str_num_array(ns[2].trim())),
     })
 );
 
 const data = await fs.readFile("./day4/sample.txt", {encoding: "ascii"});
+const cards = data.split("\n").map(parse_scratch_card);
+// console.log(cards);
 
-console.log(
-    data.split("\n").map(parse_scratch_card)
-);
+cards.forEach(
+    (card) => {
+        const wins = card.numbers.intersection(card.draw);
+        console.log(wins, Math.pow(2,wins.size-1))
+    }
+)
