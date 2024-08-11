5ff863c0b3477f2a2efc2d29b2b3fb5b0eb3db7f
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-11 14:00:50 +0100

    experimenting with fp approach

--- day4/main.ts
+++ day4/main.ts
@@ -2,13 +2,7 @@ import fs from "node:fs/promises";
 import { parse_scratch_card, wins } from "./scratchcard.ts";
 
 const data = await fs.readFile("./day4/sample.txt", {encoding: "ascii"});
-
-const cards = data
-    .split("\n")
-    .map(parse_scratch_card);
-
-let sum = cards
-    .map(wins)
-    .reduce((sum,num) => sum + num);
+const cards = data.split("\n").map(parse_scratch_card);
+const sum = cards.map(wins).reduce((acc,n) => acc + n);
 
 console.log(sum);
--- day4/scratchcard.ts
+++ day4/scratchcard.ts
@@ -6,24 +6,16 @@ export interface ScratchCard {
     numbers: Set<number>;
 }
 
-const str_num_array = (line :string): number[] => line
-    .trim()
-    .split(/\s+/)
-    .filter((l) => l.length != 0)
-    .map((l) => parseInt(l));
+const break_down =(x:string) => x.split(/^Card|:|\|/).filter((l) => l.length);
+const strnums_to_array = (line :string): number[] => line.trim().split(/\s+/).map(Number);
+const to_numbers = (x:string[]) => x.map(strnums_to_array);
+const calc_wins = (c:ScratchCard) => c.numbers.intersection(c.draw);
+const calc_score = (w:Set<number>) => w.size ? Math.pow(2,w.size-1) : 0;
+const BuildCard = (ns:number[][]): ScratchCard => ({
+    card: ns[0][0],
+    draw: new Set(ns[1]),
+    numbers: new Set(ns[2])
+});
 
-export const parse_scratch_card = (line :string): ScratchCard => pipe(
-    line
-        .split(/^Card|:|\|/)
-        .filter((l) => l.length),
-    (ns): ScratchCard => ({
-            card: str_num_array(ns[0].trim())[0],
-            draw: new Set(str_num_array(ns[1].trim())),
-            numbers: new Set(str_num_array(ns[2].trim())),
-    })
-);
-
-export const wins = (card: ScratchCard): number => pipe(
-    card.numbers.intersection(card.draw),
-    (w) => w.size ? Math.pow(2,w.size-1) : 0
-);
+export const parse_scratch_card = (line :string): ScratchCard => pipe(line, break_down, to_numbers, BuildCard);
+export const wins = (card: ScratchCard): number => pipe(card, calc_wins, calc_score);
