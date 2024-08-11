dea4d72ca257dccde99abc58ee39171d22cb0a76
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-11 10:11:21 +0100

    cosmetics

--- day4/main.ts
+++ day4/main.ts
@@ -2,7 +2,13 @@ import fs from "node:fs/promises";
 import { parse_scratch_card, wins } from "./scratchcard.ts";
 
 const data = await fs.readFile("./day4/sample.txt", {encoding: "ascii"});
-const cards = data.split("\n").map(parse_scratch_card);
 
-let sum = cards.map(wins).reduce((sum,num) => sum + num);
+const cards = data
+    .split("\n")
+    .map(parse_scratch_card);
+
+let sum = cards
+    .map(wins)
+    .reduce((sum,num) => sum + num);
+
 console.log(sum);
