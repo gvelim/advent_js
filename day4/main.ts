acbbbd2d02ccae43d67824bb40135f093618453a
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-16 18:22:46 +0100

    part 2

--- day4/main.ts
+++ day4/main.ts
@@ -1,8 +1,30 @@
 import fs from "node:fs/promises";
-import { parse_scratch_card, wins } from "./scratchcard.ts";
+import { parse_scratch_card, ScratchCard, card_score, calc_wins } from "./scratchcard.ts";
+import { pipe, HashMap, Option} from "effect";
 
-const data = await fs.readFile("./day4/sample.txt", {encoding: "ascii"});
+const data = await fs.readFile("./day4/input.txt", {encoding: "ascii"});
 const cards = data.split("\n").map(parse_scratch_card);
-const sum = cards.map(wins).reduce((acc,n) => acc + n);
+let sum = cards.map(card_score).reduce((acc,n) => acc + n);
 
-console.log(sum);
+console.time();
+console.log("Part 1 - Total wins: ",sum);
+console.timeEnd();
+
+const card_cloner = (cards: ScratchCard[]) => {
+    let h = HashMap.fromIterable(cards.map(card => [card.card,1]));
+    return function(id:number, wins: number): number {
+        const copies = h.pipe(HashMap.get(id), Option.getOrThrow);
+        while(wins > 0)
+            h = h.pipe(HashMap.modify(id + wins--, v => v + copies));
+        return copies;
+    }
+}
+
+const cc = card_cloner(cards);
+sum = cards
+    .map(c => cc(c.card, calc_wins(c).size))
+    .reduce( (acc,num) => acc + num);
+
+console.time();
+console.log("Part 2 - Total wins: ",sum);
+console.timeEnd();
--- day4/scratchcard.ts
+++ day4/scratchcard.ts
@@ -6,16 +6,16 @@ export interface ScratchCard {
     numbers: Set<number>;
 }
 
-const break_down =(x:string) => x.split(/^Card|:|\|/).filter((l) => l.length);
+const break_down = (x:string) => x.split(/^Card|:|\|/).filter((l) => l.length);
 const strnums_to_array = (line :string): number[] => line.trim().split(/\s+/).map(Number);
 const to_numbers = (x:string[]) => x.map(strnums_to_array);
-const calc_wins = (c:ScratchCard) => c.numbers.intersection(c.draw);
+export const calc_wins = (c:ScratchCard) => c.numbers.intersection(c.draw);
 const calc_score = (w:Set<number>) => w.size ? Math.pow(2,w.size-1) : 0;
-const BuildCard = (ns:number[][]): ScratchCard => ({
+const to_scratchcard = (ns:number[][]): ScratchCard => ({
     card: ns[0][0],
     draw: new Set(ns[1]),
     numbers: new Set(ns[2])
 });
 
-export const parse_scratch_card = (line :string): ScratchCard => pipe(line, break_down, to_numbers, BuildCard);
-export const wins = (card: ScratchCard): number => pipe(card, calc_wins, calc_score);
+export const parse_scratch_card = (line :string): ScratchCard => pipe(line, break_down, to_numbers, to_scratchcard);
+export const card_score = (card: ScratchCard): number => pipe(card, calc_wins, calc_score);
