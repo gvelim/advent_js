227b8692d674005da28b23c06c61a8f465746ca3
gvelim <3185416+gvelim@users.noreply.github.com>
2024-08-17 11:44:41 +0100

    fix to pure functions
    fnA( fnB ) => fn( card ) => fnB(card)

--- day4/scratchcard.ts
+++ day4/scratchcard.ts
@@ -1,32 +1,48 @@
-import { pipe, HashMap , Option } from "effect";
+import { pipe, HashMap, Option } from "effect";
+
+type CardNums = Set<number>;
 
 interface ScratchCard {
     card: number;
-    draw: Set<number>;
-    numbers: Set<number>;
+    draw: CardNums;
+    numbers: CardNums;
 }
 
-const break_down = (x:string) => x.split(/^Card|:|\|/).filter((l) => l.length);
-const strnums_to_array = (line :string): number[] => line.trim().split(/\s+/).map(Number);
-const to_numbers = (x:string[]) => x.map(strnums_to_array);
-const calc_wins = (c:ScratchCard) => c.numbers.intersection(c.draw);
-const calc_score = (w:Set<number>) => w.size ? Math.pow(2,w.size-1) : 0;
+/// Utility functions
+const str_of_nums_to_arr = (line :string): number[] => line.trim().split(/\s+/).map(Number);
+
+/// Parsing functions
+const split_parts = (x:string) => x.split(/^Card|:|\|/).filter((l) => l.length);
+const parts_to_numbers = (fn_toNum: (_:string) => number[]) => (x:string[]) => x.map(fn_toNum);
 const to_scratchcard = (ns:number[][]): ScratchCard => ({
     card: ns[0][0],
     draw: new Set(ns[1]),
     numbers: new Set(ns[2])
 });
+export const parse_scratch_card = (line :string): ScratchCard => pipe(
+    line,
+    split_parts,
+    parts_to_numbers(str_of_nums_to_arr),
+    to_scratchcard
+);
 
-export const parse_scratch_card = (line :string): ScratchCard => pipe(line, break_down, to_numbers, to_scratchcard);
-export const card_score = (card: ScratchCard): number => pipe(card, calc_wins, calc_score);
+/// Part 1 functions
+const calc_winning_nums = (c:ScratchCard): CardNums => c.numbers.intersection(c.draw);
+const calc_score = (fn_wins: (c:ScratchCard) => CardNums) => (c:ScratchCard) => {
+    const w = fn_wins(c);
+    return w.size ? Math.pow(2,w.size-1) : 0;
+}
+export const card_score = (card: ScratchCard): number => pipe(card, calc_score(calc_winning_nums));
 
+
+/// Part 2 functions
 export const card_cloner = (cards: ScratchCard[]): (c:ScratchCard) => number => {
     let h = HashMap.beginMutation(
         HashMap.fromIterable(cards.map(card => [card.card,1]))
     );
     return (card: ScratchCard): number => {
         const copies = h.pipe(HashMap.get(card.card), Option.getOrThrow);
-        let wins = calc_wins(card).size;
+        let wins = calc_winning_nums(card).size;
         while(wins > 0)
             h.pipe(HashMap.modify(card.card + wins--, v => v + copies));
         return copies;
