0a95c77a27ec6e820e2ca2397a2f79f2f43bbbe4
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-08-20 12:21:48 +0100

    effects/Array

--- day4/main.ts
+++ day4/main.ts
@@ -1,16 +1,28 @@
 import fs from "node:fs/promises";
 import { parse_scratch_card, card_score, card_cloner } from "./scratchcard.ts";
+import { pipe } from "effect/Function";
+import { Array } from "effect";
+
 
 const data = await fs.readFile("./day4/input.txt", {encoding: "ascii"});
 const cards = data.split("\n").map(parse_scratch_card);
-let sum = cards.map(card_score).reduce((acc,n) => acc + n);
+
+let sum = pipe(
+    cards,
+    Array.map(card_score),
+    Array.reduce(0,(acc,n) => acc + n)
+);
 
 console.time();
 console.log("Part 1 - Total wins: ",sum);
 console.timeEnd();
 
 const cloner = card_cloner(cards);
-sum = cards.map(cloner).reduce((acc,num) => acc + num);
+sum = pipe(
+    cards,
+    Array.map(cloner),
+    Array.reduce(0,(acc,num) => acc + num)
+);
 
 console.time();
 console.log("Part 2 - Total cards: ",sum);
