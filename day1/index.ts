2925508341c58af43b4889a3be9537bd940b5afa
gvelim <3185416+gvelim@users.noreply.github.com>
2024-09-22 17:01:01 +0100

    part1 iter protocol implementation

--- day1/index.ts
+++ day1/index.ts
@@ -1,15 +1,19 @@
 import * as fs from 'node:fs/promises';
-import {Parser, Part1ParserGen, Part2ParserGen, Part2ParserIter} from './parser.ts';
+import {ParserIter, Part1ParserGen, Part1ParserIter, Part2ParserGen, Part2ParserIter} from './parser.ts';
 
 const lines = (await fs.readFile("./day1/input.txt", { encoding: "ascii" })).split("\n");
 
-const parse_sum = (parser: Parser) => (lines: string[]): number => lines
+const parse_sum = (parser: ParserIter) => (lines: string[]): number => lines
     .map( line => Array.from(parser.parse(line)) )
     .map( nums => nums[0] ? parseInt(nums[0] + nums.pop()) : 0)
     .reduce((acc, sum) => acc + sum);
 
 console.time();
 console.log("\nPart1ParserGen Total = " + parse_sum(new Part1ParserGen())(lines));
+console.timeEnd();
+
+console.time();
+console.log("\nPart1ParserIter Total = " + parse_sum(new Part1ParserIter())(lines));
 console.timeEnd();
 
 console.time();
--- day1/parser.ts
+++ day1/parser.ts
@@ -11,11 +11,38 @@ const word_to_numeric = (buf: string): Option<string> =>
         map(i => i.toString())
     );
 
+const some_result = <T>(value:T): IteratorResult<T> => { return { done: false, value: value }}
+const no_result = (): IteratorResult<any> => { return { done: true, value: undefined }}
+
 // Given a string, return one number at a time
 // Generator implementation
 function* parse_part1(line:string): IterableIterator<string> {
-    for(const c of line) {
-        if( isDigit(c) ) yield c;
+    for(const c of line)
+        isDigit(c) ? yield c : 0;
+}
+
+// Given a string, return one number at a time
+// Iterator protocol implementation
+function parse_part1_iter(line:string): IterableIterator<string> {
+    let chars = line[Symbol.iterator]();
+    return {
+        next() {
+            let result = no_result();
+
+            let next;
+            do {
+                next = chars.next();
+                result = !next.done && isDigit(next.value) ? some_result(next.value) : result;
+            } while( result.done && !next.done)
+
+            // for(const char of chars)
+            //     if( isDigit(char) ) {
+            //         result = some_result(char);
+            //         break;
+            //     };
+            return result;
+        },
+        [Symbol.iterator]() { return this }
     }
 }
 
@@ -48,51 +75,58 @@ function parse_part2_iter(line:string): IterableIterator<string> {
     // return an iterate object; implements next() and has Symbol.iterator value set
     return {
         next(): IteratorResult<string> {
-            let ret: string = "";
-            while( ret === "" && i < line.length) {
+            let result = no_result();
+            while( result.done && i < line.length) {
                 if( isDigit(line[i]) ) {
-                    l = i+1;
-                    ret = line[i]
+                    l = i;
+                    result = some_result(line[i]);
                 } else
                     pipe(
                         line,
                         Str.substring(l,i+1),
                         word_to_numeric,
-                        map((val) => { l = i; ret = val }),
+                        map((num) => {
+                            l = i;
+                            result = some_result(num)
+                        }),
                     );
                 i++;
             }
-            return {
-                done: !ret,
-                value: ret
-            }
+            return result;
         },
         [Symbol.iterator]() { return this }
     }
 }
 
 // Parser API
-export interface Parser {
+export interface ParserIter {
     parse(line:string): IterableIterator<string>;
 }
 
 // Parser API implementation for Part 1
-export class Part1ParserGen implements Parser {
+export class Part1ParserGen implements ParserIter {
     parse(line: string): IterableIterator<string> {
         return parse_part1(line);
     }
 
 }
 
+export class Part1ParserIter implements ParserIter {
+    parse(line: string): IterableIterator<string> {
+        return parse_part1_iter(line);
+    }
+
+}
+
 // Parser API implementation for Part 2
-export class Part2ParserIter implements Parser {
+export class Part2ParserIter implements ParserIter {
     parse(line: string): IterableIterator<string> {
         return parse_part2_iter(line);
     }
 }
 
 // Parser API implementation for Part 2
-export class Part2ParserGen implements Parser {
+export class Part2ParserGen implements ParserIter {
     parse(line: string): IterableIterator<string> {
         return parse_part2_gen(line);
     }
